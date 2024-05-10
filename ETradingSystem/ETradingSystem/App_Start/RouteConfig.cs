1e27b3fb238a3ccb21c00cbd5b1327a99c74f2f4
Vikash Vishwakarma <vikashv@infinite.com>
2024-05-10 09:39:57 +0530

    redirecting and new layout's

--- ETradingSystem/ETradingSystem/App_Start/RouteConfig.cs
+++ ETradingSystem/ETradingSystem/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace ETradingSystem
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+                name: "Help",
+                url: "help",
+                defaults: new { controller = "Home", action = "Help" }
+            );
         }
     }
 }
